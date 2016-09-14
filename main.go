232c6969d95e241aa67650aee9fb0c1d7515bed1
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-09-14 09:57:20 -0700

    Modify fester to report the images and containers on the system

--- main.go
+++ main.go
@@ -1,227 +1,73 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 var (
-	reg   = flag.String("registry", "", "The registry to pull from")
-	imgs  = flag.String("images", "", "Path to a new-line delimited list of image names")
-	tag   = flag.String("tag", "", "The tag to pull")
-	outf  = flag.String("output", "", "The file to write the JSON to.")
-	files = flag.String("files", "", "A list of files that need to be included in the manifest.")
+	dockerURI = flag.String("docker-uri", "unix:///var/run/docker.sock", "The docker URI.")
+	outf      = flag.String("output", "", "The file to write the JSON to.")
+	files     = flag.String("files", "", "A list of files that need to be included in the manifest.")
 )
-
-func init() {
-	flag.Parse()
-}
-
-// VersionInfo encapsulates version info extracted from a Docker image.
-type VersionInfo struct {
-	GitRef  string `json:"git_ref"`
-	ImageID string `json:"image_id"`
-}
-
-// NewVersionInfo creates a new VersionInfo instance from info parsed out of a
-// []string.
-func NewVersionInfo(parsefrom []string, imageID string) *VersionInfo {
-	var gitref string
-	for _, p := range parsefrom {
-		if strings.HasPrefix(p, "Git-Ref: ") {
-			gitref = strings.TrimSpace(strings.TrimPrefix(p, "Git-Ref: "))
-		}
-	}
-	v := &VersionInfo{
-		GitRef:  gitref,
-		ImageID: imageID,
-	}
-	return v
-}
-
-// ImageSpecifier encapsulates a Docker image string.
-type ImageSpecifier struct {
-	Registry string
-	Image    string
-	Tag      string
-}
-
-// New returns a pointer to a new ImageSpecifier.
-func New(reg, img, tag string) *ImageSpecifier {
-	i := &ImageSpecifier{
-		Registry: reg,
-		Image:    img,
-		Tag:      tag,
-	}
-	return i
-}
-
-func (i *ImageSpecifier) String() string {
-	return fmt.Sprintf("%s/%s:%s", i.Registry, i.Image, i.Tag)
-}
-
-// Pull pulls a Docker image.
-func (i *ImageSpecifier) Pull() error {
-	cmd := exec.Command(
-		"docker",
-		"pull",
-		i.String(),
-	)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
-}
-
-// ReadLines parses a []byte into a []string based on newlines.
-func ReadLines(content []byte) []string {
-	var lines []string
-	reader := bytes.NewReader(content)
-	scanner := bufio.NewScanner(reader)
-	scanner.Split(bufio.ScanLines)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines
-}
-
-// ImageID returns the image identifier for the docker image.
-func (i *ImageSpecifier) ImageID() (string, error) {
-	var outbuf bytes.Buffer
-	outwriter := bufio.NewWriter(&outbuf)
-	cmd := exec.Command(
-		"docker",
-		"inspect",
-		"--format",
-		"{{.Id}}",
-		i.String(),
-	)
-	cmd.Stdout = outwriter
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-	err = outwriter.Flush()
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(outbuf.String()), nil
-}
-
-// Version returns the output of calling --version on the image.
-func (i *ImageSpecifier) Version() (*VersionInfo, error) {
-	var outbuf bytes.Buffer
-	outwriter := bufio.NewWriter(&outbuf)
-	cmd := exec.Command(
-		"docker",
-		"run",
-		"--rm",
-		i.String(),
-		"--version",
-	)
-	cmd.Stdout = outwriter
-	err := cmd.Run()
-	if err != nil {
-		return nil, err
-	}
-	err = outwriter.Flush()
-	if err != nil {
-		return nil, err
-	}
-	imageID, err := i.ImageID()
-	if err != nil {
-		return nil, err
-	}
-	return NewVersionInfo(ReadLines(outbuf.Bytes()), imageID), nil
-}
-
-// ReadImages reads in file and returns a []string of image names.
-func ReadImages(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-	filebytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		return nil, err
-	}
-	return ReadLines(filebytes), nil
-}
-
-// ReadFilesList reads in the list of files that need to be included in the drop
-// and returns the map.
-func ReadFilesList(filename string) (map[string]string, error) {
-	filebytes, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	retval := make(map[string]string)
-	err = json.Unmarshal(filebytes, &retval)
-	if err != nil {
-		return nil, err
-	}
-	return retval, nil
-}
 
 // OutputMap contains the info that is written out to a file.
 type OutputMap struct {
-	DropFiles    map[string]string         `json:"drop_files"`
-	DockerImages map[string][]*VersionInfo `json:"docker_images"`
+	Hostname   string            `json:"hostname"`
+	Date       string            `json:"date"`
+	Images     []types.Image     `json:"images"`
+	Containers []types.Container `json:"containers"`
 }
 
 func main() {
-	if *imgs == "" {
-		log.Fatal("--images must be set")
-	}
-	if *reg == "" {
-		log.Fatal("--registry must be set")
-	}
-	if *tag == "" {
-		log.Fatalf("--tag must be set")
-	}
-	if *outf == "" {
-		log.Fatalf("--output must be set")
-	}
-	images, err := ReadImages(*imgs)
+	flag.Parse()
+
+	//Create the docker client. Config will be blank because this apps doesn't
+	//use it and isn't creating any porklock containers.
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	d, err := client.NewClient(*dockerURI, "v1.22", nil, defaultHeaders)
 	if err != nil {
-		log.Fatalf("Error reading images: %s", err)
+		log.Fatalf("Error creating docker client: %s", err)
 	}
-	dropFiles, err := ReadFilesList(*files)
+
+	ctx := context.Background()
+
+	images, err := d.ImageList(ctx, types.ImageListOptions{All: true})
 	if err != nil {
-		log.Fatalf("Error reading files list: %s", err)
+		log.Fatal(err)
 	}
-	imageVersions := make(map[string][]*VersionInfo)
-	for _, image := range images {
-		fmt.Println(image)
-		spec := New(*reg, image, *tag)
-		err = spec.Pull()
-		if err != nil {
-			log.Fatalf("Error pulling image: %s", err)
-		}
-		v, err := spec.Version()
-		if err != nil {
-			log.Fatalf("Error getting version: %s", err)
-		}
-		imageVersions[spec.String()] = append(imageVersions[spec.String()], v)
+
+	containers, err := d.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	var hostname string
+	if hostname, err = os.Hostname(); err != nil {
+		hostname = ""
+	}
+
+	date := time.Now().Format("2006-01-02T15:04:05-07:00")
+
 	output := &OutputMap{
-		DropFiles:    dropFiles,
-		DockerImages: imageVersions,
+		Hostname:   hostname,
+		Date:       date,
+		Images:     images,
+		Containers: containers,
 	}
+
 	imgJSON, err := json.MarshalIndent(output, "", "  ")
 	if err != nil {
 		log.Fatalf("Error marshalling JSON: %s", err)
 	}
-	err = ioutil.WriteFile(*outf, imgJSON, 0644)
-	if err != nil {
-		log.Fatalf("Error writing JSON file: %s", err)
+	if _, err = os.Stdout.Write(imgJSON); err != nil {
+		log.Fatal(err)
 	}
 }
